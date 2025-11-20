cfd09db1180dbb271680b077aae2b2eeb4a0a676
stackd <144233917+toosplatched@users.noreply.github.com>
2025-11-20 23:32:21 +0800

    Create memory.js

---
+++ memory.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = "./messageMemory.json";
+
+let memory = {};
+
+// Load existing memory
+function loadMemory() {
+    if (fs.existsSync(path)) {
+        memory = JSON.parse(fs.readFileSync(path, "utf8"));
+    } else {
+        fs.writeFileSync(path, JSON.stringify({}, null, 2));
+    }
+}
+
+// Save memory back to file
+function saveMemory() {
+    fs.writeFileSync(path, JSON.stringify(memory, null, 2));
+}
+
+// Add a logged message (while keeping only last 10)
+function logMessage(channelId, memberName, message) {
+    if (!memory[channelId]) memory[channelId] = [];
+
+    memory[channelId].push({
+        memberName,
+        message
+    });
+
+    // keep only last 10
+    if (memory[channelId].length > 10) {
+        memory[channelId] = memory[channelId].slice(-10);
+    }
+
+    saveMemory();
+}
+
+module.exports = {
+    loadMemory,
+    saveMemory,
+    logMessage,
+    memory
+};
